70cc786fb151c3d75a6f3ea34dd633075a84ccb1
BlackNight Shadow <65853201+BNLOS@users.noreply.github.com>
2023-03-10 05:22:42 +0300

    Update and rename winehq.sh to winehqfocal.sh

---
+++ winehqfocal.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo mkdir -pm755 /etc/apt/keyrings
+sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/focal/winehq-focal.sources
